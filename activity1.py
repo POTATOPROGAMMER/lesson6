01a43761c400c935e73d4687ac25383b296f3776
POTATOPROGAMMER <codercode1010@gmail.com>
2024-11-11 17:53:46 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,13 @@
+letter=str (input("enter the letter"))
+if letter=="a":
+    print ("this is vowel")
+elif letter== "e":
+    print ("this is a vowel")
+elif letter== "i":
+    print ("this is a vowel")
+elif letter== "u":
+    print ("this is a vowel")
+elif letter== "o":
+    print ("this is a vowel")
+else:
+    print ("it is not a vowel")
