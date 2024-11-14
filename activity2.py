74d241092af41db046ffa63133f7c566c688500d
POTATOPROGAMMER <codercode1010@gmail.com>
2024-11-14 17:23:05 +0400

    commit2

---
+++ activity2.py
@@ -0,0 +1,24 @@
+a=10
+b=20
+if a > 0 and b > 0 : 
+    print ("a and b is greater than 0")
+else : 
+    print ("a and b is lesser than 0")
+
+
+a=-5
+b=20
+if a > 0 and b > 0 : 
+    print ("a and b is greater than 0")
+else : 
+    print ("a and b is lesser than 0")
+
+
+
+
+a=10
+b=20
+if a > 0 or b > 0 : 
+    print ("a or b is greater than 0")
+else : 
+    print ("a or b is lesser than 0")
